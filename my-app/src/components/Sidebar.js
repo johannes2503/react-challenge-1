5fd8eb766e855e8fcb1c58ca9be8b5daad8a2498
Johannes David Hreinsson <johannes2503@gmail.com>
2022-11-29 21:26:56 +0000

    Added css to component

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,7 +1,8 @@
 import React from "react"
+import css from "./css/Sidebar.module.css"
 
 function Sidebar() {
-   return <div><a>My Photos,</a><a> My Illustrations,</a><a> & My paintings</a></div>
+   return <div className= {css.sidebar} ><a>My Photos,</a><a> My Illustrations,</a><a> & My paintings</a></div>
 }
 
 export default Sidebar
